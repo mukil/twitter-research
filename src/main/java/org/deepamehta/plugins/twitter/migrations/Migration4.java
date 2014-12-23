89c8f23285a1c95ff56ff99502e24d1eaed90160
Malte Reißig <malte@mikromedia.de>
2014-12-23 15:31:26 +0100

    1.3.3 release (compatible with DM 4.4.x)

---
+++ src/main/java/org/deepamehta/plugins/twitter/migrations/Migration4.java
@@ -0,0 +1,25 @@
+package org.deepamehta.plugins.twitter.migrations;
+
+import de.deepamehta.core.Topic;
+import de.deepamehta.core.model.SimpleValue;
+import de.deepamehta.core.service.Migration;
+
+/** 
+ * This migration changes the Workspace name to "Twitter" and its uri to be simply
+ * "org.deepamehta.workspaces.twitter".
+ */
+public class Migration4 extends Migration {
+
+    @Override
+    public void run() {
+
+        // 1) Configure new user profile icon 
+        dms.getTopicType("org.deepamehta.twitter.user").getViewConfig().addSetting("dm4.webclient.view_config",
+                "dm4.webclient.icon", "/org.deepamehta.twitter-research/images/profile_twitter-32-42.png");
+        // 2) Configure new search bucket icon
+        dms.getTopicType("org.deepamehta.twitter.search").getViewConfig().addSetting("dm4.webclient.view_config",
+                "dm4.webclient.icon", "/org.deepamehta.twitter-research/images/search_twitter-32.png");
+
+    }
+
+}
